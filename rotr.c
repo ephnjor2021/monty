eff70f77a195dcf35c2651649b72e7626e718978
Ephraim <77397831+ephnjor2021@users.noreply.github.com>
2023-05-27 10:47:26 +0300

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -1,28 +1,30 @@
 #include "monty.h"
-/**
- * rotr - rotates to the right
- * @stack: monty stack
- * @line_number: line number
- */
-void rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
-	int lastVal;
 
-	UNUSED(line_number);
-	if ((*stack) != NULL)
-	{
-		tmp = *stack;
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-		lastVal = tmp->n;
-		while (tmp->prev != NULL)
-		{
-			tmp->n = tmp->prev->n;
-			tmp = tmp->prev;
-		}
-		tmp->n = lastVal;
-	}
+/**
+* rotr - rotates the last node of a stack_t stack
+* @stack: stack head
+* @line_count: line count
+*
+* Return: void
+*/
+void rotr(stack_t **stack, unsigned int line_count)
+{
+	stack_t *bottom;
+	stack_t *prev;
+
+	(void) line_count;
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	bottom = *stack;
+
+	while (bottom->next)
+		bottom = bottom->next;
+
+	prev = bottom->prev;
+	bottom->next = *stack;
+	bottom->prev = NULL;
+	prev->next = NULL;
+	(*stack)->prev = bottom;
+	*stack = bottom;
 }
