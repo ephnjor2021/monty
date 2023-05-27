8456c58e3822a1d30fa0f6275211fe6d3ee6c806
Ephraim <77397831+ephnjor2021@users.noreply.github.com>
2023-05-27 10:45:29 +0300

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -1,19 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
-/**
- * pstr - prints the string of list
- * @stack: monty stack
- * @line_number: line number to grab
- */
-void pstr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
 
-	tmp = *stack;
-	UNUSED(line_number);
-	while (tmp != NULL && tmp->n != 0 && isprint(tmp->n))
+/**
+ * pstr - prints the contents of a stack_t stack as a string
+ * @stack: stack given by main
+ * @line_cnt: line counter for error messages
+ *
+ * Return: nothing
+ */
+void pstr(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	stack_t *current = *stack;
+
+	while (current)
 	{
-		printf("%c", tmp->n);
-		tmp = tmp->next;
+		if (current->n <= 0 || current->n > 127)
+			break;
+		putchar((char) current->n);
+		current = current->next;
 	}
-	printf("\n");
+	putchar('\n');
 }
