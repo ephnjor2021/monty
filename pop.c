9ea9660de1254cdf3ee637d71f6eb3b1fad4efa5
Ephraim <77397831+ephnjor2021@users.noreply.github.com>
2023-05-27 10:44:48 +0300

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include "monty.h"
 
 /**
- * pstr - prints the contents of a stack_t stack as a string
- * @stack: stack given by main
- * @line_cnt: line counter for error messages
- *
- * Return: nothing
- */
-void pstr(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+* pop - pops the very top element of the stack
+* @stack: stack given by main in start.c
+* @line_cnt: line number for error messages
+*
+* Return: void
+*/
+void pop(stack_t **stack, unsigned int line_cnt)
 {
-	stack_t *current = *stack;
+	stack_t *tmp = NULL;
 
-	while (current)
+	if (!stack || !*stack)
 	{
-		if (current->n <= 0 || current->n > 127)
-			break;
-		putchar((char) current->n);
-		current = current->next;
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_cnt);
+		exit(EXIT_FAILURE);
 	}
-	putchar('\n');
+
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+	if (!*stack)
+		return; /* prevents errors cause next line might assign a NULL */
+	(*stack)->prev = NULL;
 }
